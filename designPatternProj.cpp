f3885252f2249273ba4a9665627483e18a4b0497
suneeshs <suneesh.sk@gmail.com>
2022-04-26 18:32:13 -0400

    Add project files.

---
+++ designPatternProj.cpp
@@ -0,0 +1,12 @@
+﻿// designPatternProj.cpp : Defines the entry point for the application.
+//
+
+#include "designPatternProj.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
